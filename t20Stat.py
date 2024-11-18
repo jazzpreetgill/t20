b6d49a34f4b8f3a68405a3c854aa0807d9c7420c
jazzpreetgill <140133292+jazzpreetgill@users.noreply.github.com>
2024-11-18 13:32:57 -0700

    Update t20Stat.py

--- t20Stat.py
+++ t20Stat.py
@@ -4,13 +4,18 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 import pandas as pd
 
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+
 page_url = 'https://www.iplt20.com/stats/2023'
 #print(page_url)
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
-#chrome_options.add_argument('--disable-dev-shm-usage')
-driver = webdriver.Chrome(options=chrome_options)
+chrome_options.add_argument('--disable-dev-shm-usage')
+#driver = webdriver.Chrome(options=chrome_options)
 driver.get(page_url)
 
 soup = BeautifulSoup(driver.page_source, 'html.parser')
